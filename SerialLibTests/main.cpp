d3d8ea0a6a3bafb29de343c01b5c9389d6036b01
davshus <david.shustin@gmail.com>
2016-08-17 18:36:50 -0400

    Completed and tested connect()
    Ready to implement into main IsoQuad project?
    
    Should've done a branch. Whatever.

--- SerialLibTests/main.cpp
+++ SerialLibTests/main.cpp
@@ -3,6 +3,7 @@
 #include <stdio.h>
 #include <conio.h>
 #include <iostream>
+#define	EXIT pause(); return 0
 using namespace std;
 void pause() {
 	printf("Press any key to continue.");
@@ -11,9 +12,8 @@ void pause() {
 }
 void printArgUsage(char* arg0) {
 	printf("Incorrect argument usage!\n Correct usage: %s <port> <baud rate>\n", arg0);
-	pause();
 }
-int connect(char *cport, char *cbaud = "9600") {
+serial::Serial* connect(char *cport, char *cbaud = "9600") {
 	string port(cport);
 	string sbaud(cbaud);
 	//Validate baud
@@ -22,26 +22,51 @@ int connect(char *cport, char *cbaud = "9600") {
 		baud = stoi(sbaud);
 	}
 	catch (out_of_range) {
-		printf("\nBaud rate was out of range!\nDefaulting to 9600.\n");
-		baud = 9600;
+		throw out_of_range("Baud rate is out of range.");
 	}
 	catch (invalid_argument) {
-		printf("\nNo valid number detected.\nDefaulting to 9600.\n");
-		baud = 9600;
+		throw invalid_argument("Baud rate is not a number.");
 	}
-	return baud;
+	if (baud < 0) {
+		throw out_of_range("Baud rate is a negative number.");
+	}
+	try {
+		serial::Serial *thisSerial = new serial::Serial(port, baud, serial::Timeout(1000));
+		return thisSerial;
+	}
+	catch (const exception& e) {
+		printf("There was a problem creating a Serial connection.\n");
+		throw exception(e.what());
+	}
 }
 int main(int argc, char *argv[]) {
 	if (argc < 2 || argc > 3) {
 		printArgUsage(argv[0]);
-		return 0;
+		EXIT;
 	}
 	if (argc == 2) {
-		printf("%i\n", connect(argv[1]));
+		try {
+			serial::Serial *SP = connect(argv[1]);
+			SP->write("test");
+			cout << SP->read(4) << endl;
+			delete SP;
+		}
+		catch (const exception& e) {
+			cout << e.what() << endl;
+			EXIT;
+		}
 	}
 	else {
-		printf("%i\n", connect(argv[1], argv[2]));
+		try {
+			serial::Serial *SP = connect(argv[1], argv[2]);
+			SP->write("test");
+			cout << SP->read(4) << endl;
+			delete SP;
+		}
+		catch (const exception& e) {
+			cout << e.what() << endl;
+			EXIT;
+		}
 	}
-	pause();
-	return 0;
+	EXIT;
 }
